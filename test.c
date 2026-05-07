83d4b00d42d59d80b0b6b4095f916d9347b1677d
JaarmaCo <williamh.jaarma@gmail.com>
2026-05-07 20:40:31 +0200

    fix: copyright info

--- test.c
+++ test.c
@@ -1,5 +1,5 @@
-
-
+///
+/// Copyright © 2026 William Jaarma (JaarmaCo@git)
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
 /// and associated documentation files (the “Software”), to deal in the Software without restriction, 
