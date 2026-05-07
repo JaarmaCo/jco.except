90d2f8b72b9d7a718f74f823c4716ad74d8bf334
JaarmaCo <williamh.jaarma@gmail.com>
2026-05-07 20:27:11 +0200

    Renamed repo to jco.except

--- test.c
+++ test.c
@@ -1,4 +1,5 @@
-#include "cexcept.h"
+#define JCOEXCEPT_IMPLEMENTATION
+#include "jco-except.h"
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -10,34 +11,34 @@ static void clean(void *arg) {
 
 int main(void) {
 
-    c_cleanup_pool(2);
-    c_except_t except;
-    c_cleanup_t cl;
+    jco_cleanup_pool(2);
+    jco_except_t except;
+    jco_cleanup_t cl;
 
-    c_cleanup_start(cl);
+    jco_cleanup_start(cl);
 
-    if (c_catch(NULL, except)) {
+    if (jco_catch(NULL, except)) {
         goto catch_exception;
     }
 
-    c_defer(cl, &clean, "Running cleanup 1");
-    c_defer_except(except, &clean, "Running cleanup 2");
+    jco_defer(cl, &clean, "Running cleanup 1");
+    jco_defer_except(except, &clean, "Running cleanup 2");
 
     srand(time(NULL));
     int value = rand();
     if (value % 2 == 1) {
-        c_throw(except, value, "Random value was odd.");
+        jco_throw(except, value, "Random value was odd.");
     }
 
     printf("Success! Value was %d\n", value);
-    c_cleanup(cl);
-    c_cleanup_except(except);
+    jco_cleanup(cl);
+    jco_cleanup_except(except);
     return 0;
 
 catch_exception:
     if (value > 1000) {
         fprintf(stderr, "Value too large, giving up...\n");
-        c_rethrow(except);
+        jco_rethrow(except);
     }
     fprintf(stderr, "%.*s\n", (int)except->message_length, except->message);
     return value;
